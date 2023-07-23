21733ac8b564ca4c60eb93f6008d810d72cc3f4d
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-07-23 22:46:05 +0300

    Finishing header with its responsive

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,14 @@
-import './style.css'
+import './style.css';
 
+/* Header with small devices */
+let toggleIcon = <HTMLDivElement>document.querySelector('.toggle-icon');
+let closeIcon = <HTMLDivElement>document.getElementById('close');
+let navBar = <HTMLMenuElement>document.querySelector('.navbar');
+
+toggleIcon.addEventListener('click', (): void => {
+  navBar.classList.add('open');
+});
+
+closeIcon.addEventListener('click', (): void => {
+  navBar.classList.remove('open');
+});
