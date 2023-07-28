37a16893298069d4b9e156cf07c3ec14a7346fce
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-07-28 15:49:47 +0300

    Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Frontend-Mentor-Challenges/typeScript-ecommerce-product-page/',
+  base: '/typeScript-product-page/',
 });
